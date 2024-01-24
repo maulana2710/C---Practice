51632a5f8bba17770f301ecc28e2492269e2dce2
maulana2710 <vickymaulana2707@gmail.com>
2024-01-24 16:00:22 +0700

    [VM] update Polimorfisme

--- contoh Program Polimorfisme.cpp
+++ contoh Program Polimorfisme.cpp
@@ -2,55 +2,56 @@
 using namespace std;
 
 class dasar {
-public :
-  virtual void setdata()=0;
-  virtual void gethitung()=0;
+public:
+  virtual void setdata() = 0;
+  virtual int gethitung() = 0;
 };
 
 class penambahan : public dasar {
-private :
-      int  x,y;
-public :
-      void setdata(){
-              cout << " Angka ke 1 :" << endl;
-              cin >> x;
-	        cout << " Angka ke 2 :" << endl;
-              cin >> y;
-       }
+private:
+  int x, y;
 
-       void gethitung(){
-                x + y;
-        }
+public:
+  void setdata() {
+    cout << " Angka ke 1 :" << endl;
+    cin >> x;
+    cout << " Angka ke 2 :" << endl;
+    cin >> y;
+  }
+
+  int gethitung() {
+    return x + y;
+  }
 };
 
 class pengurangan : public dasar {
-private :
-      int  x,y;
-public :
-      void setdata(){
-              cout << " Angka ke 1 :" << endl;
-              cin >> x;
-	        cout << " Angka ke 2 :" << endl;
-              cin >> y;
-       }
+private:
+  int x, y;
 
-       void gethitung(){
-                x - y;
-        }
+public:
+  void setdata() {
+    cout << " Angka ke 1 :" << endl;
+    cin >> x;
+    cout << " Angka ke 2 :" << endl;
+    cin >> y;
+  }
+
+  int gethitung() {
+    return x - y;
+  }
 };
 
-
 int main() {
+  penambahan q1;
+  pengurangan q2;
 
-penambahan q1 ;
-pengurangan q2;
+  cout << " Hasil Penambahan :" << endl;
+  q1.setdata();
+  cout << " a + b =  " << q1.gethitung() << endl;
 
-cout <<" Hasil Penambahan :" << endl;
-q1.setdata();
-cout << " a + b =  "  << q1.gethitung() << endl;
+  cout << " Hasil Pengurangan :" << endl;
+  q2.setdata();
+  cout << " a - b =  " << q2.gethitung() << endl;
 
-cout << " Hasil Pengurangan :" << endl;
-q2.setdata();
-cout<<" a - b =  "  << q2.gethitung() << endl;
-
+  return 0;
 }
